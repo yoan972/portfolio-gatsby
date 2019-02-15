5532bfee481a22e705fe3b292a95e2b65b818768
Yoan Hillion <yoan.hillion@hotmail.fr>
2019-02-15 23:53:45 +0100

    add airbnb prettier & eslint config

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,22 @@
-import React from 'react'
-import  logo  from '../assets/images/logoyh-white.svg'
+import React from "react";
+import logo from "../assets/images/logoyh-white.svg";
 
-const Header = class extends React.Component {
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="LOGOYH_white" />
+      </div>
+      <div className="nav-icon">
+        <div className="hamburger hamburger--slider">
+          <div className="hamburger-box">
+            <div className="hamburger-inner" />
+            <p className="hamburger-text">menu</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-    render() {
-        return (
-            <header>
-                <div className="logo">
-                    <img src={ logo } alt="LOGOYH_white"/>
-                </div>
-                <div className="nav-icon">
-                    <div className="hamburger hamburger--slider">
-                        <div className="hamburger-box">
-                            <div className="hamburger-inner"></div>
-                            <p className="hamburger-text">menu</p>
-                        </div>
-                    </div>
-                </div>
-            </header>
-        )
-    }
-}
-
-export default Header
+export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,37 +1,53 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from './Header'
+import Header from "./Header";
 
-import '../styles/styles.scss'
+import "../styles/styles.scss";
 
 const TemplateWrapper = ({ children }) => (
   <StaticQuery
     query={graphql`
       query HeadingQuery {
-          site {
-            siteMetadata {
-              title,
-              description,
-            }
+        site {
+          siteMetadata {
+            title
+            description
           }
         }
+      }
     `}
     render={data => (
       <main className="main">
         <Helmet>
           <html lang="en" />
           <title>{data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon64px-black.png" sizes="64x64" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
+          <meta
+            name="description"
+            content={data.site.siteMetadata.description}
+          />
 
-	        <meta property="og:type" content="business.business" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/img/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/img/favicon64px-black.png"
+            sizes="64x64"
+          />
+
+          <link
+            rel="mask-icon"
+            href="/img/safari-pinned-tab.svg"
+            color="#ff4400"
+          />
+          <meta name="theme-color" content="#fff" />
+
+          <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
@@ -41,6 +57,6 @@ const TemplateWrapper = ({ children }) => (
       </main>
     )}
   />
-)
+);
 
-export default TemplateWrapper
+export default TemplateWrapper;

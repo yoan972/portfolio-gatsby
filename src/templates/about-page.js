dd3380fb1b6d280f33de17dba72458a5d30462e4
Yoan Hillion <yoan.hillion@hotmail.fr>
2019-02-22 21:59:21 +0100

    searching about page preview fix

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,46 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-import Content, { HTMLContent } from "../components/Content";
+import { HTMLContent } from "../components/Content";
+import AboutPageTemplate from "./about-page-template";
 
-import download from "../assets/images/download.svg";
-import cv from "../assets/images/CV_YoanHillion.pdf";
-
-const AboutPageTemplate = ({ title, content, contentComponent }) => {
-  const PageContent = contentComponent || Content;
-
-  return (
-    <div>
-      <h1 className="presentation">
-        <span className="bold">{`${title} `}</span>
-      </h1>
-      <section className="about">
-        <PageContent content={content} />
-        <p className="about__button">
-          <a href={cv} className="form__button" download="">
-            Full CV
-          </a>
-          <img src={download} alt="download-arrow" className="download-arrow" />
-        </p>
-      </section>
-    </div>
-  );
-};
-
-export default AboutPageTemplate;
-
-AboutPageTemplate.defaultProps = {
-  content: "",
-  contentComponent: {}
-};
-
-AboutPageTemplate.propTypes = {
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string,
-  contentComponent: PropTypes.func
-};
-
-export const AboutPage = ({ data }) => {
+const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data;
 
   return (
@@ -66,6 +30,8 @@ AboutPage.propTypes = {
     })
   }).isRequired
 };
+
+export default AboutPage;
 
 export const aboutPageQuery = graphql`
   query AboutPage($id: String!) {

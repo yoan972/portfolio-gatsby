4ddeb0bc61f11112cb5a47354891d0509aee6d33
Yoan Hillion <yoan.hillion@hotmail.fr>
2019-02-22 22:17:12 +0100

    searching about page preview fix

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AboutPageTemplate from "../../templates/about-page-template";
+import { AboutPageTemplate } from "../../templates/about-page";
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
     title={entry.getIn(["data", "title"])}
+    titleHoverlined={entry.getIn(["data", "titleHoverlined"])}
     content={widgetFor("body")}
   />
 );
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,8 +2,44 @@ import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
-import { HTMLContent } from "../components/Content";
-import AboutPageTemplate from "./about-page-template";
+import Content, { HTMLContent } from "../components/Content";
+
+import download from "../assets/images/download.svg";
+import cv from "../assets/images/CV_YoanHillion.pdf";
+
+export const AboutPageTemplate = ({
+  title,
+  titleHoverlined,
+  content,
+  contentComponent
+}) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <div>
+      <h1 className="presentation">
+        <span className="bold">{`${title} `}</span>
+        <span className="hoverline">{titleHoverlined}</span>
+      </h1>
+      <section className="about">
+        <PageContent content={content} />
+        <p className="about__button">
+          <a href={cv} className="form__button" download="">
+            Full CV
+          </a>
+          <img src={download} alt="download-arrow" className="download-arrow" />
+        </p>
+      </section>
+    </div>
+  );
+};
+
+AboutPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  titleHoverlined: PropTypes.string,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func
+};
 
 const AboutPage = ({ data }) => {
   const { markdownRemark: post } = data;
@@ -13,6 +49,7 @@ const AboutPage = ({ data }) => {
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
+        titleHoverlined={post.frontmatter.titleHoverlined}
         content={post.html}
       />
     </Layout>

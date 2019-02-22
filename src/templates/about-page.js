fbd9f5ce08fb30eb1f68603dbce95d57a7aaa7f7
Yoan Hillion <yoan.hillion@hotmail.fr>
2019-02-22 20:36:23 +0100

    searching about page preview fix

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -7,19 +7,13 @@ import Content, { HTMLContent } from "../components/Content";
 import download from "../assets/images/download.svg";
 import cv from "../assets/images/CV_YoanHillion.pdf";
 
-export const AboutPageTemplate = ({
-  title,
-  titleHoverlined,
-  content,
-  contentComponent
-}) => {
+export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
     <div>
       <h1 className="presentation">
         <span className="bold">{`${title} `}</span>
-        <span className="hoverline">{titleHoverlined}</span>
       </h1>
       <section className="about">
         <PageContent content={content} />
@@ -41,7 +35,6 @@ AboutPageTemplate.defaultProps = {
 
 AboutPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
-  titleHoverlined: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func
 };
@@ -54,7 +47,6 @@ const AboutPage = ({ data }) => {
       <AboutPageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
-        titleHoverlined={post.frontmatter.titleHoverlined}
         content={post.html}
       />
     </Layout>

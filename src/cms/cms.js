7c8f26a90e85cf4563931e18cc452213d01cce44
Yoan Hillion <yoan.hillion@hotmail.fr>
2019-02-27 00:25:25 +0100

    fix config.yml indentation

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from "netlify-cms";
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
+import ProjectPagePreview from "./preview-templates/ProjectPagePreview";
 
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("project", ProjectPagePreview);
